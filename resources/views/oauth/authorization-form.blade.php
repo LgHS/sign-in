0d2ed26c99c1ca484cf11ce04467a3a4015272ae
Tharyrok <dev@tharyrok.eu>
2016-07-12 10:34:44 +0200

    Add session table and themes from authorization

--- resources/views/oauth/authorization-form.blade.php
+++ resources/views/oauth/authorization-form.blade.php
@@ -1,12 +1,29 @@
-<h2>{{$client->getName()}}</h2>
-<form method="post" action="{{route('oauth.authorize.post', $params)}}">
-    {{ csrf_field() }}
-    <input type="hidden" name="client_id" value="{{$params['client_id']}}">
-    <input type="hidden" name="redirect_uri" value="{{$params['redirect_uri']}}">
-    <input type="hidden" name="response_type" value="{{$params['response_type']}}">
-    <input type="hidden" name="state" value="{{$params['state']}}">
-    <input type="hidden" name="scope" value="{{$params['scope']}}">
+@extends('layouts.auth')
 
-    <button type="submit" name="approve" value="1">Approve</button>
-    <button type="submit" name="deny" value="1">Deny</button>
-</form>
+@section('page_title')
+    Authorisation pour {{$client->getName()}}
+@endsection
+
+@section('title')
+    L'application "{{$client->getName()}}" aimerai utiliser votre identification pour vous connecter sur sa platform.
+@endsection
+
+@section('content')
+    <form id="sign-in-form" method="POST" action="{{route('oauth.authorize.post', $params)}}">
+        {{ csrf_field() }}
+        <input type="hidden" name="client_id" value="{{$params['client_id']}}">
+        <input type="hidden" name="redirect_uri" value="{{$params['redirect_uri']}}">
+        <input type="hidden" name="response_type" value="{{$params['response_type']}}">
+        <input type="hidden" name="state" value="{{$params['state']}}">
+        <input type="hidden" name="scope" value="{{$params['scope']}}">
+
+
+        <p class="form-submit">
+            <button type="submit" class="btn btn-success" name="approve" value="1">Accepter</button>
+        </p>
+
+        <p class="form-submit">
+            <button type="submit" class="btn btn-danger" name="deny" value="1">Refuser</button>
+        </p>
+    </form>
+@endsection
