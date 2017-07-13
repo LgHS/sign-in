ae00818bc7ed575a83866c670fdb67555cc65757
john <john@3kd.be>
2017-07-13 12:44:48 +0200

    add middleware for admin namespace

--- routes/admin.php
+++ routes/admin.php
@@ -12,57 +12,22 @@ Route::group(['prefix' => 'admin', 'middleware' => ['permission:manage-members']
     Route::delete('paymentTypes/{paymentTypes}', ['as' => 'admin.paymentTypes.destroy', 'uses' => 'PaymentTypeController@destroy']);
     Route::get('paymentTypes/{paymentTypes}', ['as' => 'admin.paymentTypes.show', 'uses' => 'PaymentTypeController@show']);
     Route::get('paymentTypes/{paymentTypes}/edit', ['as' => 'admin.paymentTypes.edit', 'uses' => 'PaymentTypeController@edit']);
+
+    Route::get('admin/transactionTypes', ['as'=> 'admin.transactionTypes.index', 'uses' => 'TransactionTypeController@index']);
+    Route::post('admin/transactionTypes', ['as'=> 'admin.transactionTypes.store', 'uses' => 'TransactionTypeController@store']);
+    Route::get('admin/transactionTypes/create', ['as'=> 'admin.transactionTypes.create', 'uses' => 'TransactionTypeController@create']);
+    Route::put('admin/transactionTypes/{transactionTypes}', ['as'=> 'admin.transactionTypes.update', 'uses' => 'TransactionTypeController@update']);
+    Route::patch('admin/transactionTypes/{transactionTypes}', ['as'=> 'admin.transactionTypes.update', 'uses' => 'TransactionTypeController@update']);
+    Route::delete('admin/transactionTypes/{transactionTypes}', ['as'=> 'admin.transactionTypes.destroy', 'uses' => 'TransactionTypeController@destroy']);
+    Route::get('admin/transactionTypes/{transactionTypes}', ['as'=> 'admin.transactionTypes.show', 'uses' => 'TransactionTypeController@show']);
+    Route::get('admin/transactionTypes/{transactionTypes}/edit', ['as'=> 'admin.transactionTypes.edit', 'uses' => 'TransactionTypeController@edit']);
+
+    Route::get('admin/transactions', ['as'=> 'admin.transactions.index', 'uses' => 'TransactionController@index']);
+    Route::post('admin/transactions', ['as'=> 'admin.transactions.store', 'uses' => 'TransactionController@store']);
+    Route::get('admin/transactions/create', ['as'=> 'admin.transactions.create', 'uses' => 'TransactionController@create']);
+    Route::put('admin/transactions/{transactions}', ['as'=> 'admin.transactions.update', 'uses' => 'TransactionController@update']);
+    Route::patch('admin/transactions/{transactions}', ['as'=> 'admin.transactions.update', 'uses' => 'TransactionController@update']);
+    Route::delete('admin/transactions/{transactions}', ['as'=> 'admin.transactions.destroy', 'uses' => 'TransactionController@destroy']);
+    Route::get('admin/transactions/{transactions}', ['as'=> 'admin.transactions.show', 'uses' => 'TransactionController@show']);
+    Route::get('admin/transactions/{transactions}/edit', ['as'=> 'admin.transactions.edit', 'uses' => 'TransactionController@edit']);
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-Route::get('admin/transactionTypes', ['as'=> 'admin.transactionTypes.index', 'uses' => 'TransactionTypeController@index']);
-Route::post('admin/transactionTypes', ['as'=> 'admin.transactionTypes.store', 'uses' => 'TransactionTypeController@store']);
-Route::get('admin/transactionTypes/create', ['as'=> 'admin.transactionTypes.create', 'uses' => 'TransactionTypeController@create']);
-Route::put('admin/transactionTypes/{transactionTypes}', ['as'=> 'admin.transactionTypes.update', 'uses' => 'TransactionTypeController@update']);
-Route::patch('admin/transactionTypes/{transactionTypes}', ['as'=> 'admin.transactionTypes.update', 'uses' => 'TransactionTypeController@update']);
-Route::delete('admin/transactionTypes/{transactionTypes}', ['as'=> 'admin.transactionTypes.destroy', 'uses' => 'TransactionTypeController@destroy']);
-Route::get('admin/transactionTypes/{transactionTypes}', ['as'=> 'admin.transactionTypes.show', 'uses' => 'TransactionTypeController@show']);
-Route::get('admin/transactionTypes/{transactionTypes}/edit', ['as'=> 'admin.transactionTypes.edit', 'uses' => 'TransactionTypeController@edit']);
-
-
-
-
-
-
-
-
-Route::get('admin/transactions', ['as'=> 'admin.transactions.index', 'uses' => 'TransactionController@index']);
-Route::post('admin/transactions', ['as'=> 'admin.transactions.store', 'uses' => 'TransactionController@store']);
-Route::get('admin/transactions/create', ['as'=> 'admin.transactions.create', 'uses' => 'TransactionController@create']);
-Route::put('admin/transactions/{transactions}', ['as'=> 'admin.transactions.update', 'uses' => 'TransactionController@update']);
-Route::patch('admin/transactions/{transactions}', ['as'=> 'admin.transactions.update', 'uses' => 'TransactionController@update']);
-Route::delete('admin/transactions/{transactions}', ['as'=> 'admin.transactions.destroy', 'uses' => 'TransactionController@destroy']);
-Route::get('admin/transactions/{transactions}', ['as'=> 'admin.transactions.show', 'uses' => 'TransactionController@show']);
-Route::get('admin/transactions/{transactions}/edit', ['as'=> 'admin.transactions.edit', 'uses' => 'TransactionController@edit']);
